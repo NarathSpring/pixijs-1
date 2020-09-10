54b9e1fe90efee22dbf75a28b057ce7b0f1721fe
NarathSpring <valentine.xu@yahoo.com>
2020-09-10 18:57:28 +0800

    add keyevent

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import TilingSprite from "./lib/createTilingSprite";
 import MapGenerater from "./lib/MapGenerater";
 import BoxGenerater from "./lib/BoxGenerater";
 
+const player = require("../public/assets/explorer.png").default;
+
 function init() {
   const app = new PIXI.Application({
     width: 512,
@@ -10,10 +12,65 @@ function init() {
     view: <HTMLCanvasElement>document.getElementById("app")
   });
 
+  const explorer = new PIXI.Sprite(PIXI.Texture.from(player));
+  explorer.position.set(112, 160);
+
+
+  let left = keyboard(37),
+    up = keyboard(38),
+    right = keyboard(39),
+    down = keyboard(40);
+  
+
+
   const map = new MapGenerater();
   const box = new BoxGenerater();
   app.stage.addChild(map);
+  app.stage.addChild(explorer);
   app.stage.addChild(box);
+}
+
+interface myKey {
+  code: number;
+  isDown: boolean;
+  isUp: boolean;
+  press: any;
+  release: any;
+  downHandler: Function;
+  upHandler: Function;
+}
+
+function keyboard(keyCode: number) {
+  let key = {} as myKey;
+  key.code = keyCode;
+  key.isDown = false;
+  key.isUp = true;
+  key.press = undefined;
+  key.release = undefined;
+  //The `downHandler`
+  key.downHandler = (event) => {
+    if (event.keyCode === key.code) {
+      if (key.isUp && key.press) key.press();
+      key.isDown = true;
+      key.isUp = false;
+    }
+    event.preventDefault();
+  };
+
+  //The `upHandler`
+  key.upHandler = (event) => {
+    if (event.keyCode === key.code) {
+      if (key.isDown && key.release) key.release();
+      key.isDown = false;
+      key.isUp = true;
+    }
+    event.preventDefault();
+  };
+
+  //Attach event listeners
+  window.addEventListener("keydown", key.downHandler.bind(key), false);
+  window.addEventListener("keyup", key.upHandler.bind(key), false);
+  return key;
 }
 
 init();
